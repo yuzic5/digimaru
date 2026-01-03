5944190fd9f5e498bebcc0e8ab472a829644d861
YujiAoki <yuzic5@gmail.com>
2026-01-03 21:59:05 +0900

    feat: integrate Partytown for Google Analytics and update site URL in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    // site はご自身の URL（https://digimaru.pages.dev/）に変更しておくと、sitemap 生成が正しく行われます
+    site: 'https://digimaru.pages.dev/', 
+    integrations: [
+        mdx(), 
+        sitemap(), 
+        partytown({
+            // config を追加し、GA4 のデータレイヤーを Web ワーカーへ転送するように設定します
+            config: {
+                forward: ["dataLayer.push"],
+            },
+        }),
+    ],
 });
